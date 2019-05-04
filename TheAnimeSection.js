335c0b4d70293f63bcd6adda925b43c70e7a0b2c
Malachi Perrin <44848986+malachiperrin@users.noreply.github.com>
2019-05-04 18:40:39 -0400

    Add files via upload

--- TheAnimeSection.js
+++ TheAnimeSection.js
@@ -2,3 +2,14 @@
 //Author: Malachi Perrin
 
 "use-strict";
+
+//Button events
+
+document.getElementById("button").addEventListener('click', function() {
+  document.querySelector('.bg-modal').style.display = "flex";
+
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+  document.querySelector('.bg-modal').style.display = 'none';
+})
